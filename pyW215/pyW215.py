d6a0e71f846a85418380b3125f8063f5b77d31f2
Christian Fredborg Braedstrup <christian@fredborg-braedstrup.dk>
2016-02-20 15:13:21 +0100

    Initial commit of library

---
+++ pyW215/pyW215.py
@@ -0,0 +1,271 @@
+
+from urllib.request import Request, urlopen
+import xml.etree.ElementTree as ET
+import hmac
+import time
+
+class SmartPlug(object):
+    """
+    Class to access a D-Link Smart Plug Switch W215
+
+    Usage example when used as library:
+    p = SmartPlug("192.168.0.10", ('admin', '1234'))
+
+    # change state of plug
+    p.state = "OFF"
+    p.state = "ON"
+
+    # query and print current state of plug
+    print(p.state)
+
+    Note: 
+    The library is greatly inspired by the javascript library by @bikerp (https://github.com/bikerp).
+    Class layout is inspired by @rkabadi (https://github.com/rkabadi) for the Edimax Smart plug.
+    """
+
+    def __init__(self, ip, password, user = "admin"):
+        """
+        Create a new SmartPlug instance identified by the given URL and password.
+
+        :rtype : object
+        :param host: The IP/hostname of the SmartPlug. E.g. '192.168.0.10'
+        :param password: Password to authenticate with the plug. Located on the plug.
+        :param user: Username for the plug. Default is admin.
+        """
+        self.url = "http://{}/HNAP1/".format(ip)
+        self.user = user
+        self.password = password
+
+    def moduleParameters(self, module):
+        """Returns moduleID XML.
+
+        :type module: str
+        :param module: module number/ID
+        :return XML string with moduleID
+        """
+        return '''<ModuleID>{}</ModuleID>'''.format(module)
+
+    def controlParameters(self, module, status):
+        """Returns control parameters as XML.
+        
+        :type module: str
+        :type status: str
+        :param module: The module number/ID
+        :param status: The state to set (i.e. true (on) or false (off))
+        :return XML string to join with payload
+        """
+        return '''{}<NickName>Socket 1</NickName><Description>Socket 1</Description>
+                  <OPStatus>{}</OPStatus>'''.format(self.moduleParameters(module), status)
+
+    def radioParameters(self, radio):
+        """Returns RadioID as XML.
+        
+        :type radio: str
+        :param radio: Radio number/ID
+        """
+        return '''<RadioID>{}</RadioID>'''.format(radio)
+
+
+    def requestBody(self, Action, params):
+        """Returns the request payload for an action as XML>.
+
+        :type Action: str
+        :type params: str
+        :param Action: Which action to perform
+        :param params: Any parameters required for request
+        :return XML payload for request
+        """
+        return '''<?xml version="1.0" encoding="UTF-8"?>
+        <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+        <soap:Body>
+            <{} xmlns="http://purenetworks.com/HNAP1/">
+            {}
+        </{}>
+        </soap:Body>
+        </soap:Envelope>
+               '''.format(Action, params, Action)
+
+    def SOAPAction(self, Action, responseElement, params = ""):
+        """Generate the SOAP action call.
+
+        :type Action: str
+        :type responseElement: str
+        :type params: str
+        :param Action: The action to perform on the device
+        :param responseElement: The XML element that is returned upon success
+        :param params: Any additional parameters required for performing request (i.e. RadioID, moduleID, ect)
+        :return: Text enclosed in responseElement brackets
+        """
+        # Authenticate client
+        auth = self.auth()
+        payload = self.requestBody(Action, params)
+
+        # Timestamp in microseconds
+        time_stamp = str(round(time.time()/1e6))
+
+        action_url = '"http://purenetworks.com/HNAP1/{}"'.format(Action)
+        AUTHKey = hmac.new(auth[0].encode(), (time_stamp+action_url).encode()).hexdigest().upper() + " " + time_stamp
+
+        headers = {'Content-Type' : '"text/xml; charset=utf-8"',
+                   'SOAPAction': '"http://purenetworks.com/HNAP1/{}"'.format(Action),
+                   'HNAP_AUTH' : '{}'.format(AUTHKey),
+                   'Cookie' : '"uid={}"'.format(auth[1])}
+
+        response = urlopen(Request(self.url, payload.encode(), headers))
+        xmlData = response.read().decode()
+        root = ET.fromstring(xmlData)
+
+        # Get value from device
+        value = root.find('.//{http://purenetworks.com/HNAP1/}%s' % (responseElement)).text
+
+        if value is None:
+            raise Exception("Could not find %s in response." % responseElement)
+
+        return value
+
+    @property
+    def current_consumption(self):
+        """Get the current power consumption in Watt."""
+        res = self.SOAPAction('GetCurrentPowerConsumption', 'CurrentConsumption', self.moduleParameters("2"))
+
+        try:
+            float(res)
+        except ValueError:
+            raise Exception("Failed to retrieve current power consumption from SmartPlug")
+        
+        return res
+    @property
+    def total_consumption(self):
+        """Get the total power consumpuntion in the device lifetime."""
+        res = self.SOAPAction("GetPMWarningThreshold", "TotalConsumption", self.moduleParameters("2"))
+
+        try:
+            float(res)
+        except ValueError:
+            raise Exception("Failed to retrieve total power consumption from SmartPlug")
+
+        return res
+
+    @property
+    def temperature(self):
+        """Get the device temperature in celsius."""
+        return self.SOAPAction('GetCurrentTemperature', 'CurrentTemperature', self.moduleParameters("3"))
+
+    @property
+    def state(self):
+        """Get the device state (i.e. ON or OFF)."""
+        response =  self.SOAPAction('GetSocketSettings', 'OPStatus', self.moduleParameters("1"))
+        if response.lower() == 'true':
+            return "ON"
+        elif response.lower() == 'false':
+            return "OFF"
+        else:
+            raise Exception("Unknown state %s returned" % str(response.lower))
+
+    @state.setter
+    def state(self, value):
+        """Set device state.
+
+        :type value: str
+        :param value: Future state (either ON or OFF)
+        """
+        if value.upper() == 'ON':
+            return self.SOAPAction('SetSocketSettings', 'SetSocketSettingsResult', self.controlParameters("1", "true"))
+        elif value.upper() == 'OFF':
+            return self.SOAPAction('SetSocketSettings', 'SetSocketSettingsResult', self.controlParameters("1", "false"))
+        else:
+            raise TypeError("State %s is not valid." % str(value))
+
+    def auth(self):
+        """Authenticate using the SOAP interface.
+
+        Authentication is a two-step process. First a initial payload
+        is sent to the device requesting additional login information in the form
+        of a publickey, a challenge string and a cookie.
+        These values are then hashed by a MD5 algorithm producing a privatekey
+        used for the header and a hashed password for the XML payload.
+        
+        If everything is accepted the XML returned will contain a LoginResult tag with the
+        string 'success'.
+
+        See https://github.com/bikerp/dsp-w215-hnap/wiki/Authentication-process for more information.
+        """
+
+        payload = self.initial_auth_payload()
+
+        # Build initial header
+        headers = {'Content-Type' : '"text/xml; charset=utf-8"',
+           'SOAPAction': '"http://purenetworks.com/HNAP1/Login"'}
+
+        # Request privatekey, cookie and challenge
+        response = urlopen(Request(self.url, payload, headers))
+        xmlData = response.read().decode()
+        root = ET.fromstring(xmlData)
+
+        # Find responses
+        Challenge = root.find('.//{http://purenetworks.com/HNAP1/}Challenge').text
+        Cookie = root.find('.//{http://purenetworks.com/HNAP1/}Cookie').text
+        Publickey = root.find('.//{http://purenetworks.com/HNAP1/}PublicKey').text
+
+        if Challenge == None or Cookie == None or Publickey == None:
+            raise Exception("Failed to receive initial authentication from smartplug.")
+
+        # Generate hash responses
+        PrivateKey = hmac.new((Publickey+self.password).encode(), (Challenge).encode()).hexdigest().upper()
+        login_pwd = hmac.new(PrivateKey.encode(), Challenge.encode()).hexdigest().upper()
+
+        response_payload = self.auth_payload(login_pwd)
+        # Build response to initial request
+        headers = {'Content-Type' : '"text/xml; charset=utf-8"',
+           'SOAPAction': '"http://purenetworks.com/HNAP1/Login"',
+           'HNAP_AUTH' : '"{}"'.format(PrivateKey),
+           'Cookie' : '"uid={}"'.format(Cookie)}
+        response = urlopen(Request(self.url, response_payload, headers))
+        xmlData = response.read().decode()
+        root = ET.fromstring(xmlData)
+
+        # Find responses
+        login_status = root.find('.//{http://purenetworks.com/HNAP1/}LoginResult').text.lower()
+        
+        if login_status != "success":
+            raise Exception("Failed to authenticate with SmartPlug")
+                    
+        return (PrivateKey, Cookie)
+
+    def initial_auth_payload(self):
+        """Return the initial authentication payload."""
+
+        return b'''<?xml version="1.0" encoding="utf-8"?>
+        <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+        <soap:Body>
+        <Login xmlns="http://purenetworks.com/HNAP1/">
+        <Action>request</Action>
+        <Username>admin</Username>
+        <LoginPassword/>
+        <Captcha/>
+        </Login>
+        </soap:Body>
+        </soap:Envelope>
+        '''
+    
+    def auth_payload(self, login_pwd):
+        """Generate a new payload containing generated hash information.
+
+        :type login_pwd: str
+        :param login_pwd: hashed password generated by the auth function.
+        """
+
+        payload = '''<?xml version="1.0" encoding="utf-8"?>
+        <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+        <soap:Body>
+        <Login xmlns="http://purenetworks.com/HNAP1/">
+        <Action>login</Action>
+        <Username>{}</Username>
+        <LoginPassword>{}</LoginPassword>
+        <Captcha/>
+        </Login>
+        </soap:Body>
+        </soap:Envelope>
+        '''.format(self.user, login_pwd)
+
+        return payload.encode()
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyW215',
+            version='0.1',
+            description='Interface for d-link W215 Smart Plugs.',
+            url='https://github.com/linuxchristian/pyW215',
+            author='Christian Fredborg Brædstrup',
+            author_email='christian@fredborg-braedstrup.dk',
+            license='MIT',
+            packages=['pyW215'],
+            zip_safe=False)
