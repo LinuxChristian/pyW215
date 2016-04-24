f812f49b0c923370507b70f125a59a62d0070efc
Christian Fredborg Braedstrup <christian@fredborg-braedstrup.dk>
2016-04-24 20:28:13 +0200

    Version update to 0.1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pyW215',
-            version='0.1',
+            version='0.1.1',
             description='Interface for d-link W215 Smart Plugs.',
             url='https://github.com/linuxchristian/pyW215',
             author='Christian Fredborg Brædstrup',
