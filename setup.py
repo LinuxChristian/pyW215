3e717a2a77dcf18f9a281e68462b5809010e1835
Christian Fredborg Braedstrup <christian@fredborg-braedstrup.dk>
2017-07-11 10:11:44 +0200

    Prepare for publication on pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
+# -*- coding: utf-8 -*-
 from setuptools import setup
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(name='pyW215',
-            version='0.4',
-            description='Interface for d-link W215 Smart Plugs.',
-            url='https://github.com/linuxchristian/pyW215',
-            author='Christian Juncker Brædstrup',
-            author_email='christian@fredborg-braedstrup.dk',
-            license='MIT',
-            packages=['pyW215'],
-            install_requires=[],
-            zip_safe=False)
+      version='0.4',
+      description='Interface for d-link W215 Smart Plugs.',
+      long_description=long_description,
+      url='https://github.com/linuxchristian/pyW215',
+      author='Christian Juncker Brædstrup',
+      author_email='christian@junckerbraedstrup.dk',
+      license='MIT',
+      keywords='D-Link W215 W110 Smartplug',
+      packages=['pyW215'],
+      install_requires=[],
+      zip_safe=False)
